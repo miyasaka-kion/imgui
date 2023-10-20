310eb2898733ea68c5ac5ff2a1e4f8e8d6d59986
Miyasaka Kion <68810213+miyasaka-kion@users.noreply.github.com>
2023-10-20 16:51:03 +0800

    modified build script

---
+++ examples/example_sdl2_sdlrenderer2/run.sh
@@ -0,0 +1,1 @@
+c++ -std=c++20 $(sdl2-config --cflags) -I .. -I ../.. -I ../../backends main.cpp ../../backends/imgui_impl_sdl2.cpp ../../backends/imgui_impl_sdlrenderer2.cpp ../../imgui*.cpp $(sdl2-config --libs) -framework OpenGl
